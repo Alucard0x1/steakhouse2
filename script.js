5b0fdb8f595c1418c2d6677b729b1d668ee3e2b5
Alucard0x1 <lombokweb@gmail.com>
2024-12-05 21:21:21 +0700

    Initial commit from your app

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add interactivity to the website
+    const reservationButton = document.querySelector('button');
+    reservationButton.addEventListener('click', () => {
+      alert('Reservation made!');
+    });
